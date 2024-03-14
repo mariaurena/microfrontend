71e8e99dbc1934b0a9d2172e46c3e0a80cc05d27
Maria <maria.urena.25c@gmail.com>
2024-03-14 18:44:22 +0100

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,27 @@
 import React from "react"
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
 import MarketingApp from "./components/MarketingApp"
 import Header from "./components/Header"
+
+const generateClassName = createGenerateClassName({
+
+    // para los estilos (css)
+    // cambiamos el prefijo por defecto que material asigna a los id (jss) por ma
+    // solo en container para que no colisione con los demás proyectos
+    productionPrefix: 'co', 
+})
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={ generateClassName }>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+
+    // para los estilos (css)
+    // cambiamos el prefijo por defecto que material asigna a los id (jss) por ma
+    // solo en marketing para que no colisione con los demás proyectos
+    productionPrefix: 'ma', 
+})
+
 export default() => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={ generateClassName }>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/pricing" component = { Pricing }></Route>
