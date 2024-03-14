4d225bc43356bbf1f0b1088a1484a41553bc4818
Maria <maria.urena.25c@gmail.com>
2024-03-14 10:41:50 +0100

    added publicpath

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'  // ruta que tenemos en nuestro S3 bucket
     },
     plugins: [
         new ModuleFederationPlugin({
