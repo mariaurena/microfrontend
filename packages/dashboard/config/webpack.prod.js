2f667d222d372b830349a468a5bdce9ce7ff1385
Maria <maria.urena.25c@gmail.com>
2024-03-19 09:44:06 +0100

    Added dashboard subapp

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -11,10 +11,10 @@ const prodConfig = {
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'auth',
+            name: 'dashboard',
             filename: 'remoteEntry.js',
             exposes: {
-                './AuthApp': './src/bootstrap'
+                './DashboardApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
         })
